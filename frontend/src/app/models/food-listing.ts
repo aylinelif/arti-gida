d561eb0a422774b1f180bb6d3418cd53b8e78afd
Aylin Elif <aylinelifgokdemir1784@gmail.com>
2026-05-07 16:02:15 +0300

    düzenleme: son arayüz optimizasyonları ve kod temizliği yapıldı

---
+++ frontend/src/app/models/food-listing.ts
@@ -0,0 +1,10 @@
+export interface FoodListing {
+  id: number;
+  establishmentName: string;
+  title: string;
+  quantity: number;
+  pickupTime: string;
+  aiCategory: string;
+  aiShelfLife: string;
+  imageUrl: string;
+}
